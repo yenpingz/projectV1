0c9645cbc416532c27628ae582db15aa2ab4179c
yenpingz <32888882+yenpingz@users.noreply.github.com>
2017-10-27 15:42:20 +0800

    1.0.1
    版型初稿

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/animate.css" rel="stylesheet" type="text/css">
 
   </head>
 <body>
@@ -36,22 +37,22 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="index.php">TRAVELFUNS</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">公司經營理念</a></li>
+        <li><a href="#">公司經營理念</a></li>
         <li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">國外旅遊<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#1">日本</a></li>
+            <li><a href="foreign.php">日本</a></li>
             <li><a href="#2">大陸</a></li>
             <li><a href="#3">東南亞</a></li>
             <li><a href="#4">歐洲</a></li>
           </ul>
         </li>
-        <li><a href="#4">國內旅遊</a></li>
-        <li><a href="#5">Page 3</a></li>
+        <li><a href="domestic.php">國內旅遊</a></li>
+        <li><a href="#5">聯絡方式</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#6"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
@@ -62,43 +63,115 @@
 </nav>
 
 </div>
+<div style="width:100%;">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
 
-<div class="jumbotron text-center">
-  <h1>My First Bootstrap Page</h1>
-  <p>Resize this responsive page to see the effect!</p>
-</div>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="test.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
 
-<div class="container" style="clear: both;">
-  <div class="row">
-    <div class="col-sm-4">
-      <a href="#8"><img src="test.jpg" style="height:225px; weight:150px;" alt=""></a>
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      <div class="item">
+        <img src="test.jpg" alt="Chicago" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="test.jpg" alt="New york" style="width:100%;">
+      </div>
     </div>
-    <div class="col-sm-4">
-      <img src="test.jpg" style="height:225px; weight:150px;" alt="">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
-    <div class="col-sm-4">
-      <img src="test.jpg" style="height:225px; weight:150px;" alt="">
-      <h3>Column 3</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 </div>
 
-<div class="container" style="clear: both;">
-  <div class="row">
-    <div class="col-sm-4"></div>
-    <div class="col-sm-4">
-<div class="fb-page test" data-href="https://www.facebook.com/%E9%81%8A%E6%A8%82%E7%AF%80-149193725823228/?modal=admin_todo_tour" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/%E9%81%8A%E6%A8%82%E7%AF%80-149193725823228/?modal=admin_todo_tour" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/%E9%81%8A%E6%A8%82%E7%AF%80-149193725823228/?modal=admin_todo_tour">遊樂節</a></blockquote></div>
+
+
+<div class="jumbotron text-center">
+  <h1 class="bounceInDown">My First Bootstrap Page</h1>
+  <p>Resize this responsive page to see the effect!</p>
 </div>
-<div class="col-sm-4"></div>
-</div>
-</div>
+
+  <div class="container" style="clear: both;">
+    <div class="row">
+      <div class="col-sm-4">
+        <a href="#8"><img src="test.jpg" style="height:225px; weight:150px;" alt=""></a>
+        <h3>Column 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+      <div class="col-sm-4">
+        <img src="test.jpg" style="height:225px; weight:150px;" alt="">
+        <h3>Column 2</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+      <div class="col-sm-4">
+        <img src="test.jpg" style="height:225px; weight:150px;" alt="">
+        <h3>Column 3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container" style="clear: both;">
+    <div class="row">
+      <div class="col-sm-4">
+        <a href="#8"><img src="test.jpg" style="height:225px; weight:150px;" alt=""></a>
+        <h3>Column 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+      <div class="col-sm-4">
+        <img src="test.jpg" style="height:225px; weight:150px;" alt="">
+        <h3>Column 2</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+      <div class="col-sm-4">
+        <img src="test.jpg" style="height:225px; weight:150px;" alt="">
+        <h3>Column 3</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      </div>
+    </div>
+  </div>
+
+
+          <div class="container" style="clear: both;">
+                <div class="row">
+                  <div class="col-sm-4"></div>
+                  <div class="col-sm-4">
+              <div class="fb-page test" data-href="https://www.facebook.com/%E9%81%8A%E6%A8%82%E7%AF%80-149193725823228/?modal=admin_todo_tour" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/%E9%81%8A%E6%A8%82%E7%AF%80-149193725823228/?modal=admin_todo_tour" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/%E9%81%8A%E6%A8%82%E7%AF%80-149193725823228/?modal=admin_todo_tour">遊樂節</a></blockquote></div>
+              </div>
+                <div class="col-sm-4"></div>
+              </div>
+          </div>
+
+    <footer class="section section-primary" id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <h1>TRAVELFUNS</h1>
+            <p contenteditable="true">版權所有 © 2017 &nbsp; St WHISKYBAR All Right Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
 </body>
 </html>
